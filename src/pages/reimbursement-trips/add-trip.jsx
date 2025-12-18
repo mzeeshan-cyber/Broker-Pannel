1db6507e6361e67b5aa29a15b4f99d4e25350675
zeeshan-cbt <muhammadzeeshan@companiontransport.com>
2025-12-18 15:40:10 +0500

    add allowance and reimbursement trip add issues fixed

--- src/pages/reimbursement-trips/add-trip.jsx
+++ src/pages/reimbursement-trips/add-trip.jsx
@@ -6,14 +6,17 @@ import SearchableSelect from 'components/common/SearchableSelect';
 import TripDetail from 'components/pages/reimbursement-trips/trip-detail';
 import { Call, ShieldTick, Sms, User } from 'iconsax-react';
 import AddTripForm from './add-trip-form';
-import { Box } from '@mui/material';
 import CircularLoader from 'components/common/loader/CircularLoader';
+import EmptyCityCard from 'components/common/empty-card';
+import { HiOutlineShieldExclamation } from "react-icons/hi";
 
 export const AddReimbursementTrip = () => {
     const [selected, setSelected] = useState(null);
-    const [patientsData, setPatientsData] = useState([])
-    const [showForm, setShowForm] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
+    const [patientsData, setPatientsData] = useState([]);
+    const [showForm, setShowForm] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [allowance, setAllowance] = useState();
+
     const getPatients = async () => {
         setIsLoading(true)
         try {
@@ -37,6 +40,34 @@ export const AddReimbursementTrip = () => {
                 message: error?.message || 'Something went wrong. Please try again.',
                 variant: 'alert',
                 alert: { color: 'error' }
+            });
+        }
+    };
+    const handleGetAllowance = async () => {
+        try {
+            const response = await fetcher('fetch-allowances');
+            if (response.status === true) {
+                setAllowance(response.data)
+            }
+            else {
+                openSnackbar({
+                    open: true,
+                    message: 'Allowances are not fetched!',
+                    variant: 'alert',
+                    alert: {
+                        color: 'error'
+                    }
+                });
+            }
+        }
+        catch (error) {
+            openSnackbar({
+                open: true,
+                message: error.message || 'Allowances are not found, Add allownces!',
+                variant: 'alert',
+                alert: {
+                    color: 'error'
+                }
             });
         }
     };
@@ -64,16 +95,23 @@ export const AddReimbursementTrip = () => {
     }));
     const SelectedPatient = filteredPatient[0];
     useEffect(() => {
+        handleGetAllowance();
         getPatients();
     }, [])
     return (
-        isLoading ?
+        isLoading || !allowance ?
             <CircularLoader text='Loading patients...' height='40vh' /> :
             <MainCard title="Add New Trip">
-                <SearchableSelect options={mappedPatients} placeholder="Select Patient" selected={selected} setSelected={setSelected} />
-                <TripDetail filteredPatient={filteredPatient} setShowForm={setShowForm} />
-                {showForm &&
-                    <AddTripForm mappedPatients={SelectedPatient} />
+                {allowance?
+                    <>
+                        <SearchableSelect options={mappedPatients} placeholder="Select Patient" selected={selected} setSelected={setSelected} />
+                        <TripDetail filteredPatient={filteredPatient} setShowForm={setShowForm} />
+                        {showForm &&
+                            <AddTripForm mappedPatients={SelectedPatient} allowance={allowance} />
+                        }
+                    </>
+                    :
+                    <EmptyCityCard message = "No Allowances found!" description="To add new Trip first and allowances." icon={<HiOutlineShieldExclamation size={24} />} buttonText="Add allowances" link="/reimbursement-trips/allowances"/>
                 }
             </MainCard>
     )
