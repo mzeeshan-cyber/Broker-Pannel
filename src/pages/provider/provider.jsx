665b742333be28fc0da5ae5688267dd233815443
zeeshan-cbt <muhammadzeeshan@companiontransport.com>
2025-12-18 20:52:54 +0500

    reimbursement trip issues fixed

--- src/pages/provider/provider.jsx
+++ src/pages/provider/provider.jsx
@@ -2,37 +2,72 @@
 import Grid from '@mui/material/Grid';
 import { openSnackbar } from 'api/snackbar';
 import { useEffect, useState } from 'react';
-import Loader from 'components/Loader';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
 import { fetcher, fetcherDelete, fetcherUpdate } from 'utils/axios';
-import { loading, providerData, providerPaginationData } from 'store/reducers/providerSlice';
+import { filterValue, loading, providerData, providerPaginationData } from 'store/reducers/providerSlice';
 import CommonTable from 'pages/tables/react-table/common-table';
 import { columns } from 'pages/tables/broker-tables/providers/providerTableColumn';
 import ProviderButtonsOnTable from 'components/pages/providers/ProviderButtonsOnTable';
-import { resetFilter } from 'store/reducers/patientSlice';
 import CircularLoader from 'components/common/loader/CircularLoader';
 
-// ==============================|| DASHBOARD - DEFAULT ||============================== //
+
 export default function Provider() {
   const [pageSize, setPageSize] = useState(10);
   const [page, setPage] = useState(1);
   const dispatch = useDispatch();
+  const [filters, setFilters] = useState({});
 
-  const getProviderData = async (values) => {
+  const getProviderData = async (values = {}) => {
     dispatch(loading(true));
-    const params = {
-      name: values?.name,
-      company_phone: values?.phone_number,
-      email: values?.email,
-    }
-    const response = await fetcher(["/providers", { params }]);
+    const query = {
+      name: values?.name || '',
+      company_phone: values?.phone_number || '',
+      email: values?.email || '',
+      page,
+      per_page: pageSize
+    };
+    setFilters(query);
+    const response = await fetcher([
+      "/providers",
+      { params: query }
+    ]);
     if (response.status === true) {
+      dispatch(providerData(response.data.data));
+      dispatch(filterValue(values));
+      dispatch(providerPaginationData(response.data));
       dispatch(loading(false));
-      dispatch(providerData(response?.data?.data));
-      dispatch(providerPaginationData(response?.data));
     }
   };
+  const handleChangePerPage = async (event) => {
+    const per_page = Number(event.target.value);
+
+    setPageSize(per_page);
+    setPage(1);
+
+    const response = await fetcher([
+      "/providers",
+      { params: { ...filters, page: 1, per_page } }
+    ]);
+
+    if (response.status === true) {
+      dispatch(providerData(response.data.data));
+      dispatch(providerPaginationData(response.data));
+    }
+  };
+  const handleChangePagination = async (event, value) => {
+    setPage(value);
+
+    const response = await fetcher([
+      "/providers",
+      { params: { ...filters, page: value, per_page: pageSize } }
+    ]);
+
+    if (response.status === true) {
+      dispatch(providerData(response.data.data));
+      dispatch(providerPaginationData(response.data));
+    }
+  };
+
   const deleteProvider = async (id) => {
     dispatch(loading(true));
     const response = await fetcherDelete([`/provider/${id}`]);
@@ -65,42 +100,14 @@ export default function Provider() {
       });
     }
   }
-
-  // Pagination
-  const handleChangePerPage = async (event) => {
-    setPageSize(Number(event.target.value));
-    const per_page = Number(event.target.value);
-    const response = await fetcher([`/providers?per_page=${per_page}`]);
-    if (response.status === true) {
-      dispatch(providerData(response?.data?.data))
-      openSnackbar({
-        open: true,
-        message: response.message || 'data is fetched',
-        variant: 'alert',
-        alert: { color: 'success' }
-      });
-    }
-  };
-  const handleChangePagination = async (event, value) => {
-    const eventValue = event.target.value;
-    setPage(eventValue ? eventValue : value);
-    const response = await fetcher([`/providers?page=${eventValue ? eventValue : value}`]);
-    if (response.status === true) {
-      dispatch(providerData(response?.data?.data))
-      openSnackbar({
-        open: true,
-        message: response.message || 'data is fetched',
-        variant: 'alert',
-        alert: { color: 'success' }
-      });
-    }
-  };
-
   const providerState = useSelector(state => state?.provider)
   const isLoading = providerState.loading;
   const providerStateData = providerState.providerData;
 
-  useEffect(() => { getProviderData() }, [])
+  useEffect(() => {
+    getProviderData({}, page, pageSize);
+  }, []);
+
   return (
     <Grid>
       {isLoading ?
@@ -117,9 +124,9 @@ export default function Provider() {
           handleChangePagination={handleChangePagination}
           handleDelete={deleteProvider}
           handleUpdate={updateProvider}
-          stackontable={<ProviderButtonsOnTable handleGetData={getProviderData} tableType="provider" />}
+          stackontable={<ProviderButtonsOnTable handleGetData={getProviderData} tableType="provider" filters={filters} />}
           tableName="provider"
-          
+
         />
       }
     </Grid>
